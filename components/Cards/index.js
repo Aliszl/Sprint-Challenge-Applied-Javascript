a0f789baeb0d741a4c2d531e612dd0aae72f9eb2
Alison Law <alisparklaw@gmail.com>
2019-11-01 14:31:29 +0000

    y

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,44 +17,9 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
-function createCard(cardInfo) {
-    // 1- Create HTML markup
 
-    const card = document.createElement('div');
-    const headline = document.createElement('div');
-    const authorDiv = document.createElement('div');
-    const imgContainer = document.createElement('div');
-    const authorImg = document.createElement('img');
-    const authorName = document.createElement('span');
+// Create array of relevant data from Axios
 
-    // 2- Define HTML structure
-    card.append(headline);
-    card.append(authorDiv);
-    authorDiv.append(imgContainer);
-    imgContainer.append(authorImg);
-    authorDiv.append(authorName);
-    // 3- Add some class names
-    card.classList.add('card');
-    headline.classList.add('headline');
-    authorDiv.classList.add('author');
-    imgContainer.classList.add('img-container')
-
-
-    // 4- Add some content!
-
-    // headline.textContent = data.articles.bootstrap.headline;
-    // authorImg.setAttribute('href',data.articles.bootstrap.authorphoto);
-    // authorName.textContent = data.articles.bootstrap.authorName;
-    headline.textContent = data.articles.javascript.headline;
-    authorImg.setAttribute('href', data.articles.javascript.authorPhoto);
-    authorName.textContent = data.articles.javascript.authorName;
-
-    // 5- Add functionality
-    console.log(card);
-    return card;
-}
-// const newCard = document.querySelector('.cards-container');
-// newCard.append(createCard(cardInfo));
 const authorArray = [];
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
     .then((response) => {
@@ -62,29 +27,80 @@ axios.get('https://lambda-times-backend.herokuapp.com/articles')
         for (let i = 0; i < response.data.articles.javascript.length; i++) {
             authorArray.push(response.data.articles.javascript[i])
         }
+        authorArray.forEach(args => {
+            newCard.append(createCard(args.headline, args.authorPhoto, args.authorName));
+        
+        })
         console.log(authorArray.length);
         console.log(authorArray);
-
     })
-    .then((array) => {
-
-        for (let i = 0; i < authorArray.length; i++) {
-            let search = 'https://lambda-times-backend.herokuapp.com/articles/javascript/';
-            search += array[i];
-            axios.get(search);
-            console.log(search);
-            // .then((X) =>{
-            //     newCard.append(createCard(X.data));
-            // })
-            // const newCard = document.querySelector('.cards-container');
-        }
-
-    })
-
     .catch(error => {
         console.log(error);
     });
+console.log('Here',authorArray);
 
+
+function createCard(headline, authorPhoto, authorName) {
+    // 1- Create HTML markup
+
+    const card = document.createElement('div');
+    const headLine = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const authorImg = document.createElement('img');
+    const name = document.createElement('span');
+
+    // 2- Define HTML structure
+    card.append(headLine);
+    card.append(authorDiv);
+    authorDiv.append(imgContainer);
+    imgContainer.append(authorImg);
+    authorDiv.append(name);
+
+    // 3- Add some class names
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgContainer.classList.add('img-container')
+
+
+    // 4- Add some content!
+    // headLine.textContent = data.articles.javascript.headline;
+    // authorImg.setAttribute('href', data.articles.javascript.authorPhoto);
+    // authorImg.textContent = data.articles.javascript.authorPhoto;
+    // Name.textContent = data.articles.javascript.authorName;
+    headLine.textContent = headline;
+    authorImg.setAttribute('src', authorPhoto);
+    authorImg.textContent = authorPhoto;
+    name.textContent = authorName;
+
+    return card;
+}
+
+// Create cards and populate with data from authorArray
+const newCard = document.querySelector('.cards-container');
+console.log(newCard);
+// test 
+// newCard.append(createCard(authorArray[0].headline, authorArray[0].headline, dummy3));
+authorArray.forEach(args => {
+    newCard.append(createCard(args.headline, args.authorPhoto, args.authorName));
+
+})
+// Failed code
+// .then((array) => {
+
+//     for (let i = 0; i < authorArray.length; i++) {
+//         let search = 'https://lambda-times-backend.herokuapp.com/articles/javascript/';
+//         search += array[i];
+//         axios.get(search);
+//         console.log(search);
+//         // .then((X) =>{
+//         //     newCard.append(createCard(X.data));
+//         // })
+//         // const newCard = document.querySelector('.cards-container');
+//     }
+
+// })
 
 
 // const newCard = document.querySelector('.cards-container');
