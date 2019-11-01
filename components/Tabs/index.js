d0d1aea2f5aa9aa94c84e6f91494c6e798fbc8c1
Alison Law <alisparklaw@gmail.com>
2019-11-01 11:00:31 +0000

    trending Topics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,37 +7,66 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-  .then(response => {
-    // Looping through the response we receive from the server
-    console.log(response);
-//     response.data.topics.forEach(top => {
-//       // Creating new components based on the API data!
-//       accordion.append(createPanel(`Doggo`, top));
-//     });
-  })
-  .catch(error => {
-    console.log(error);
-  });
 
-  function createTabComponent(topic) {
+
+function createTabComponent(topic) {
     // 1- Create HTML markup
-    
-    
-    const tab =document.createElement('div');
-     // 2- Define HTML structure
-    
-    //  .append();
+
+
+    const tab = document.createElement('div');
+    // 2- Define HTML structure
+    //  title.append(tab);
     // 3- Add some class names
-  
-    // tab.classList.add('tab');
-     // 4- Add some content!
-    
+
+    tab.classList.add('tab');
+    // 4- Add some content!
+
+    // tab.textContent = topic;
     tab.textContent = topic;
+    // tab.textContent = topic.data;
+    // tab.textContent = topic.data.topics;
+   
+    
     // 5- Add functionality
     console.log(tab);
-        return tab;
-    }
-    const newTab = document.querySelector('.topics');
-    newTab.append(createTabComponent(`dummy tab`));
-    // const header = document.querySelector('.');
+    return tab;
+}
+// const newTab = document.querySelector('.topics');
+
+// const header = document.querySelector('.');
+const topicsArray = [];
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((response) => {
+            console.log(response);
+
+            response.data.topics.forEach(topic => {
+
+                const newTab = document.querySelector('.topics');
+                newTab.append(createTabComponent(topic));
+            });
+            console.log(response.data.topics);
+// newTab.append(createTabComponent(response));
+              // return response.data.topics[i];
+                // return response.data[i].topics;
+
+    //     for(let i = 0; i < response.data.length; i ++){
+    //         topicsArray.push(
+    //     }
+    //     console.log(topicsArray);
+    //     return topicsArray;    
+    // })
+    // .then((response) => {
+        
+    // for(let i = 0; i < topicsArray.length; i ++){
+    //     newTab.append(createTabComponent(response));
+    // }
+    // console.log(topicsArray);
+    // return topicsArray;    
+})
+
+
+    .catch(error => {
+        console.log(error);
+    });
+    
+            
