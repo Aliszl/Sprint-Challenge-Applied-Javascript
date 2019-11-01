4eb2772f964004b8a9e79a3a919108f62e24d02d
Alison Law <alisparklaw@gmail.com>
2019-11-01 09:08:29 +0000

    tab set up with dummy tab, axios pull request set up

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,37 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
+    // Looping through the response we receive from the server
+    console.log(response);
+//     response.data.topics.forEach(top => {
+//       // Creating new components based on the API data!
+//       accordion.append(createPanel(`Doggo`, top));
+//     });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+  function createTabComponent(topic) {
+    // 1- Create HTML markup
+    
+    
+    const tab =document.createElement('div');
+     // 2- Define HTML structure
+    
+    //  .append();
+    // 3- Add some class names
+  
+    // tab.classList.add('tab');
+     // 4- Add some content!
+    
+    tab.textContent = topic;
+    // 5- Add functionality
+    console.log(tab);
+        return tab;
+    }
+    const newTab = document.querySelector('.topics');
+    newTab.append(createTabComponent(`dummy tab`));
+    // const header = document.querySelector('.');
